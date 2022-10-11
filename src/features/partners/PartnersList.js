760f71c9e5f7afeb2b61a6bd783dbbd9ac95bc77
Ossama Qureshi <ossamasq1@outlook.com>
2022-10-11 13:17:17 -0400

    Redux for All

--- src/features/partners/PartnersList.js
+++ src/features/partners/PartnersList.js
@@ -1,23 +1,21 @@
 import { Col } from "reactstrap";
 import Partner from "./Partner";
 import { selectAllPartners } from "./partnersSlice";
+import { useSelector } from "react-redux";
 
 const PartnersList = () => {
-    const partners = selectAllPartners();
-    return (
-        <Col className='mt-4'>
-            {
-                 partners.map((partner) => {
-                    return (
-                        <div className='d-flex mb-5' and key={partner.id}>
-                            <Partner partner={partner} />
-                        </div>
-                    )
-                 })
-            }
-        </Col>
-    )
+  const partners = useSelector(selectAllPartners);
+  return (
+    <Col className="mt-4">
+      {partners.map((partner) => {
+        return (
+          <div className="d-flex mb-5" and key={partner.id}>
+            <Partner partner={partner} />
+          </div>
+        );
+      })}
+    </Col>
+  );
 };
 
 export default PartnersList;
-
